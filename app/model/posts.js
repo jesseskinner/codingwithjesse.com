91853b4fccb22b99dc237ec51dadb2fa6397fe45
Jesse Skinner <jesse@thefutureoftheweb.com>
2015-12-29 14:19:01 -0500

    Part 9: remove a post

--- app/model/posts.js
+++ app/model/posts.js
@@ -2,7 +2,7 @@ var database = require('./database');
 
 exports.createTable = function () {
 	return database.query(`
-		CREATE TABLE posts (
+		CREATE TABLE IF NOT EXISTS posts (
 			id int NOT NULL AUTO_INCREMENT,
 			title varchar(255) NOT NULL,
 			body TEXT NOT NULL,
@@ -13,8 +13,10 @@ exports.createTable = function () {
 	`);
 };
 
-exports.dropTable = function () {
-	return database.query("DROP TABLE IF EXISTS posts");
+exports.deleteAll = function () {
+	return database.query("DELETE FROM posts").then(function () {
+		return database.query("ALTER TABLE posts AUTO_INCREMENT = 1");
+	});
 };
 
 exports.add = function (params) {
@@ -22,4 +24,12 @@ exports.add = function (params) {
 		.then(function (result) {
 			return result.insertId;
 		});
+};
+
+exports.remove = function (id) {
+	return database.query("DELETE FROM posts WHERE ?", {
+		id: id
+	}).then(function (result) {
+		return result.affectedRows;
+	});
 };
--- test/app/model/test_posts.js
+++ test/app/model/test_posts.js
@@ -7,21 +7,38 @@ var posts = require('../../../app/model/posts');
 var database = require('../../../app/model/database');
 var config = require('../../config');
 
-database.init(config.database);
-
-beforeEach(function () {
-	return posts.dropTable().then(function () {
+describe('posts', function () {
+	before(function () {
+		database.init(config.database);
 		return posts.createTable();
 	});
-});
 
-describe('posts', function () {
-	it('should add a post', function () {
-		return expect(posts.add({
-			title: 'Test',
-			body: 'Hello',
-			slug: 'test'
+	beforeEach(function () {
+		return posts.deleteAll();
+	});
 
-		})).to.eventually.equal(1);
+	describe('#add', function () {
+		it('should add a post', function () {
+			return expect(posts.add({
+				title: 'Test',
+				body: 'Hello',
+				slug: 'test'
+
+			})).to.eventually.equal(1);
+		});
+	});
+
+	describe('#remove', function () {
+		it('should remove a post', function () {
+			return expect(
+				posts.add({
+					title: 'Test',
+					body: 'Hello',
+					slug: 'test'
+				}).then(function (id) {
+					return posts.remove(id);
+				})
+			).to.eventually.equal(1);
+		});
 	});
 });
