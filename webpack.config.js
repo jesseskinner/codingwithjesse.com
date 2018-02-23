a8f7f10800a42c3ed64a1011b5a6bb6afa95acb8
Jesse Skinner <jesse@thefutureoftheweb.com>
2018-02-23 12:52:33 -0500

    NEW: update webpack to v3, babel etc. to latest versions

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,35 @@ var webpack = require('webpack');
 
 module.exports = {
 	context: __dirname + '/src',
-    entry: "./admin/js/index.js",
-    output: {
-        path: __dirname + '/build/admin',
-        filename: "admin.bundle.js"
-    },
-    module: {
-        loaders: [
+	entry: './admin/js/index.js',
+	output: {
+		path: __dirname + '/build/admin',
+		filename: 'admin.bundle.js'
+	},
+	module: {
+		rules: [
 			{
 				test: /.js$/,
 				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015', 'react']
-				}
+				exclude: /node_modules/
 			},
 
-            { test: /\.css$/, loader: "style!css" },
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
 
-            { test: /\.(woff|woff2|eot|ttf|svg)$/, loader: 'url' }
-        ]
-    },
-    plugins: [
-	    new webpack.DefinePlugin({
-	      "process.env": {
-	        NODE_ENV: JSON.stringify("production")
-	      }
-	    })
-    ]
+			{
+				test: /\.(woff|woff2|eot|ttf|svg)$/,
+				loader: 'url-loader'
+			}
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		})
+	]
 };
