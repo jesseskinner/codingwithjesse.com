b2ad6b2238dd75bfaa2fde18d5f4dc1c33a4550a
Jesse Skinner <jesse@thefutureoftheweb.com>
2018-05-25 16:27:50 +0200

    NEW: stylize admin slightly

--- src/admin/js/view/posts/list.js
+++ src/admin/js/view/posts/list.js
@@ -3,7 +3,9 @@ import postsStore from '../../model/posts';
 
 export default class PostList extends Component {
 	componentDidMount() {
-		this._unsubscribe = postsStore.getState(posts => this.setState({ posts }));
+		this._unsubscribe = postsStore.getState(posts =>
+			this.setState({ posts })
+		);
 	}
 
 	componentWillUnmount() {
@@ -11,27 +13,31 @@ export default class PostList extends Component {
 	}
 
 	render() {
-		const posts = this.state && this.state.posts || [];
+		const posts = (this.state && this.state.posts) || [];
 
 		return (
 			<div>
-				List page
+				<h1>Posts</h1>
 
-				<table>
-					<tr><th>Title</th><th>Date</th></tr>
-					{
-						posts.map(post => (
-							<tr>
-								<td>{post.title}</td>
-								<td>{post.posted_at}</td>
-								<td><a href={"/admin/posts/edit/" + post.id}>Edit</a></td>
-							</tr>
-						))
-					}
+				<table class="table">
+					<tr>
+						<th scope="col">Title</th>
+						<th scope="col">Date</th>
+					</tr>
+					{posts.map(post => (
+						<tr>
+							<td>{post.title}</td>
+							<td>{post.posted_at}</td>
+							<td>
+								<a href={'/admin/posts/edit/' + post.id}>
+									Edit
+								</a>
+							</td>
+						</tr>
+					))}
 				</table>
-
 				<a href="/admin/posts/add">Add new post</a>
 			</div>
 		);
 	}
-};
+}
