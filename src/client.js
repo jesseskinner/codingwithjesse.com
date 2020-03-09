ce698ebf8b0909f30535c9c8ea9cc7ff63d2496c
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-03-09 23:09:44 +0100

    FIX: re-enable client-side routing. I'm assume the Twitter embeds were the cause of all problems.

--- src/client.js
+++ src/client.js
@@ -1,11 +1,11 @@
 import * as sapper from '@sapper/app';
 
 // disable client-side routing
-addEventListener('click', event => {
-	if (event.target.nodeName === 'A') {
-		event.target.rel = 'external';
-	}
-});
+// addEventListener('click', event => {
+// 	if (event.target.nodeName === 'A') {
+// 		event.target.rel = 'external';
+// 	}
+// });
 
 sapper.start({
 	target: document.querySelector('body'),
