6c4772ea3bbca6df93c6fbdfc45ab7149eca2ed6
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-08-01 13:02:10 -0400

    FIX: add post button at top of admin listing, fix undefineds in new post form

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -1,34 +1,37 @@
 <script context="module">
-  export async function preload(page, session) {
-    const res = await this.fetch("/admin/api/posts");
-    const posts = await res.json();
+	export async function preload(page, session) {
+		const res = await this.fetch('/admin/api/posts');
+		const posts = await res.json();
 
-    return { posts };
-  }
+		return { posts };
+	}
 </script>
 
 <script>
-  import Template from "./_AdminTemplate.svelte";
-  export let posts;
+	import Template from './_AdminTemplate.svelte';
+	export let posts;
 </script>
 
 <Template>
-  <h1>Posts</h1>
-  <table class="table">
-    <tr>
-      <th scope="col">Title</th>
-      <th scope="col">Date</th>
-    </tr>
-    {#each posts as { title, posted_at, id }}
-      <tr>
-        <td>{title}</td>
-        <td>{posted_at}</td>
-        <td>
-          <a href="/admin/posts/edit/{id}">Edit</a>
-        </td>
-      </tr>
-    {/each}
-  </table>
+	<h1>Posts</h1>
 
-  <a href="/admin/posts/add">Add new post</a>
+	<a href="/admin/posts/add">Add new post</a>
+
+	<table class="table">
+		<tr>
+			<th scope="col">Title</th>
+			<th scope="col">Date</th>
+		</tr>
+		{#each posts as { title, posted_at, id }}
+			<tr>
+				<td>{title}</td>
+				<td>{posted_at}</td>
+				<td>
+					<a href="/admin/posts/edit/{id}">Edit</a>
+				</td>
+			</tr>
+		{/each}
+	</table>
+
+	<a href="/admin/posts/add">Add new post</a>
 </Template>
--- src/routes/admin/posts/_PostForm.svelte
+++ src/routes/admin/posts/_PostForm.svelte
@@ -1,74 +1,81 @@
 <script>
-  export let post = {};
+	export let post = {
+		title: '',
+		markdown: '',
+		html: '',
+		tags: '',
+		category: 0,
+	};
 
-  const categoriesFetch = getCategories();
+	const categoriesFetch = getCategories();
 
-  let slug = post.slug || "";
+	let slug = post.slug || '';
 
-  function setSlug(text) {
-    slug = text
-      .trim()
-      .toLowerCase()
-      .replace(/[^a-z0-9]+/g, "-");
-  }
+	function setSlug(text) {
+		slug = text
+			.trim()
+			.toLowerCase()
+			.replace(/[^a-z0-9]+/g, '-');
+	}
 
-  async function getCategories() {
-    if (typeof fetch !== "undefined") {
-      const res = await fetch("/admin/categories");
-      return res.json();
-    }
-  }
+	async function getCategories() {
+		if (typeof fetch !== 'undefined') {
+			const res = await fetch('/admin/categories');
+			return res.json();
+		}
+	}
 </script>
 
 <fieldset class="form-group">
-  <label htmlFor="title">Title</label>
-  <input
-    name="title"
-    class="form-control"
-    value={post.title}
-    on:input={event => setSlug(event.target.value)} />
+	<label htmlFor="title">Title</label>
+	<input
+		name="title"
+		class="form-control"
+		value={post.title}
+		on:input={(event) => setSlug(event.target.value)} />
 </fieldset>
 <fieldset class="form-group">
-  <label htmlFor="slug">Slug</label>
-  <input
-    name="slug"
-    class="form-control"
-    value={slug}
-    on:input={event => setSlug(event.target.value)} />
+	<label htmlFor="slug">Slug</label>
+	<input
+		name="slug"
+		class="form-control"
+		value={slug}
+		on:input={(event) => setSlug(event.target.value)} />
 </fieldset>
 <fieldset class="form-group">
-  <label htmlFor="markdown">Body</label>
-  <textarea
-    name="markdown"
-    class="form-control"
-    rows="10"
-    value={post.markdown} />
+	<label htmlFor="markdown">Body</label>
+	<textarea
+		name="markdown"
+		class="form-control"
+		rows="10"
+		value={post.markdown} />
 </fieldset>
 <fieldset class="form-group">
-  <label htmlFor="markdown">HTML</label>
-  <textarea
-    name="html"
-    class="form-control"
-    rows="10"
-    value={post.markdown ? '' : post.html} />
+	<label htmlFor="markdown">HTML</label>
+	<textarea
+		name="html"
+		class="form-control"
+		rows="10"
+		value={post.markdown ? '' : post.html} />
 </fieldset>
 <fieldset>
-  <label for="category">Category:</label>
-  {#await categoriesFetch}
-    ...
-  {:then categories}
-    <select name="category">
-      {#each categories as c}
-        <option value={c.id} selected={c.id === post.category}>
-          {c.category}
-        </option>
-      {/each}
-    </select>
-  {:catch e}
-    Error: {e.message}
-  {/await}
+	<label for="category">Category:</label>
+	{#await categoriesFetch}
+		...
+	{:then categories}
+		<select name="category">
+			<option>Select a category...</option>
+			{#each categories as c}
+				<option value={c.id} selected={c.id === post.category}>
+					{c.category}
+				</option>
+			{/each}
+		</select>
+	{:catch e}
+		Error: {e.message}
+	{/await}
 </fieldset>
 <fieldset class="form-group">
-  <label for="tags">Tags</label>
-  <input name="tags" class="form-control" value={post.tags} />
+	<label for="tags">Tags</label>
+	<input name="tags" class="form-control" value={post.tags} />
 </fieldset>
