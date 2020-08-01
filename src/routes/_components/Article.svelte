a786c7b8b235215011bc8a0122adc154902b444e
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-08-01 13:21:53 -0400

    FIX: formatting of separator causing horizontal scrolling

--- src/routes/_components/Article.svelte
+++ src/routes/_components/Article.svelte
@@ -1,88 +1,87 @@
 <script>
-  import { formatDate } from "../_date.js";
+	import { formatDate } from '../_date.js';
 
-  export let title = "";
-  export let date = false;
-  export let link = false;
+	export let title = '';
+	export let date = false;
+	export let link = false;
 </script>
 
 <style>
-  article {
-    max-width: 800px;
-    position: relative;
-  }
+	article {
+		max-width: 800px;
+		position: relative;
+		padding-bottom: 2.5em;
+		margin-bottom: 2.5em;
+		border-bottom: 1px solid #eee;
+	}
 
-  h1 {
-    min-width: 70vw;
-    margin: 0 0 .25em;
-    font-size: 32px;
-  }
+	article:last-child {
+		border-bottom: 0;
+		margin-bottom: 0;
+	}
 
-  .date {
-    font-weight: bold;
-    margin: 0 0 2em;
-  }
+	h1 {
+		min-width: 70vw;
+		margin: 0 0 0.25em;
+		font-size: 32px;
+	}
 
-  article :global(pre) {
-    padding: 2em 5vw;
-    box-sizing: border-box;
-    overflow: auto;
-    background: #29292e;
-    color: #ffffff;
-  }
+	.date {
+		font-weight: bold;
+		margin: 0 0 2em;
+	}
 
-  article :global(pre),
-  .separator {
-    margin: 0 -2.5vw 0 -5vw;
-  }
+	article :global(pre) {
+		padding: 2em 5vw;
+		box-sizing: border-box;
+		overflow: auto;
+		background: #29292e;
+		color: #ffffff;
+	}
 
-  article :global(h2) {
-    font-size: 150%;
-    margin-top: 1em;
-  }
+	article :global(pre) {
+		margin: 0 -2.5vw 0 -5vw;
+	}
 
-  article :global(h3) {
-    font-size: 120%;
-  }
+	article :global(h2) {
+		font-size: 150%;
+		margin-top: 1em;
+	}
 
-  article :global(h4),
-  article :global(h5),
-  article :global(h6) {
-    font-size: 100%;
-  }
+	article :global(h3) {
+		font-size: 120%;
+	}
 
-  .separator {
-    border-bottom: 1px solid #ccc;
-  }
+	article :global(h4),
+	article :global(h5),
+	article :global(h6) {
+		font-size: 100%;
+	}
 
-  @media (min-width: 1000px) {
-    article :global(pre) {
-      padding: 2em 15vw;
-    }
-    article :global(pre),
-    .separator {
-      margin: 0 0 0 -15vw;
-    }
-  }
+	@media (min-width: 1000px) {
+		article :global(pre) {
+			padding: 2em 15vw;
+		}
+		article :global(pre) {
+			margin: 0 0 0 -15vw;
+		}
+	}
 
-  @media (min-width: 1500px) {
-    article :global(pre),
-    .separator {
-      margin: 0 -10vw 0 -15vw;
-    }
-  }
+	@media (min-width: 1500px) {
+		article :global(pre) {
+			margin: 0 -10vw 0 -15vw;
+		}
+	}
 </style>
 
 <article>
-  <h1>
-    {#if link}
-      <a href={link}>{title}</a>
-    {:else}{title}{/if}
-  </h1>
-  {#if date}
-    <div class="date">{formatDate(date)}</div>
-  {/if}
-  <slot />
+	<h1>
+		{#if link}
+			<a href={link}>{title}</a>
+		{:else}{title}{/if}
+	</h1>
+	{#if date}
+		<div class="date">{formatDate(date)}</div>
+	{/if}
+	<slot />
 </article>
-
-<div class="separator" />
--- src/routes/blog/page/[page].svelte
+++ src/routes/blog/page/[page].svelte
@@ -1,48 +1,54 @@
 <script context="module">
-  export async function preload(page, session) {
-    const res = await this.fetch(`/blog/page/${page.params.page}.json`);
-    const posts = await res.json();
+	export async function preload(page, session) {
+		const res = await this.fetch(`/blog/page/${page.params.page}.json`);
+		const posts = await res.json();
 
-    return { posts, page: +page.params.page };
-  }
+		return { posts, page: +page.params.page };
+	}
 </script>
 
 <script>
-  import Template from "../../_Template.svelte";
-  import Post from "../../_components/Post.svelte";
-  export let posts;
-  export let page;
+	import Template from '../../_Template.svelte';
+	import Post from '../../_components/Post.svelte';
+	export let posts;
+	export let page;
 </script>
 
 <style>
-  .pagination {
-    margin-bottom: 2em;
-    text-align: center;
-  }
+	.pagination {
+		margin-bottom: 2em;
+		text-align: center;
+	}
 
-  .older {
-    float: left;
-  }
+	.older {
+		float: left;
+	}
 
-  .newer {
-    float: right;
-  }
+	.newer {
+		float: right;
+	}
 </style>
 
 <Template>
-  {#each posts as post}
-    <Post {post} />
-  {/each}
+	<div>
+		{#each posts as post}
+			<Post {post} />
+		{/each}
+	</div>
 
-  <section class="pagination">
-    {#if posts.length === 5}
-      <a class="older" href={`/blog/page/${page + 1}`}>&lt;&lt; older posts</a>
-    {/if}
-    {#if page <= 2}
-      <a class="newer" href="/">newer posts &gt;&gt;</a>
-    {:else}
-      <a class="newer" href={`/blog/page/${page - 1}`}>newer posts &gt;&gt;</a>
-    {/if}
-    <a href="/blog/all">All posts</a>
-  </section>
+	<section class="pagination">
+		{#if posts.length === 5}
+			<a class="older" href={`/blog/page/${page + 1}`}>
+				&lt;&lt; older posts
+			</a>
+		{/if}
+		{#if page <= 2}
+			<a class="newer" href="/">newer posts &gt;&gt;</a>
+		{:else}
+			<a class="newer" href={`/blog/page/${page - 1}`}>
+				newer posts &gt;&gt;
+			</a>
+		{/if}
+		<a href="/blog/all">All posts</a>
+	</section>
 </Template>
