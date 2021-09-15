4e9206aecce73ea2e9d0881655dd9f5da31cf355
Jesse Skinner <jesse@thefutureoftheweb.com>
2021-09-15 14:06:27 -0400

    NEW: add CodePen.io support to blog posts

--- src/routes/_components/Post.svelte
+++ src/routes/_components/Post.svelte
@@ -4,7 +4,41 @@
 
 	export let post;
 	export let link = true;
+
+	$: loadCodePen(post);
+
+	function loadCodePen() {
+		if (typeof window === 'object' && post && post.html && post.html.indexOf('codepen.io')) {
+			post.html = post.html.replace(
+				/<a href="[^"]+codepen.io[^"]+\/([a-z0-9]+)">[^<]+<\/a>/gi,
+				'<p class="codepen" data-height="500" data-default-tab="js,result" data-slug-hash="$1" data-editable="true" data-user="jesseskinner" style="height: 500px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"></p>'
+			);
+
+			const script = document.createElement('script');
+			script.async = true;
+			script.src = 'https://cpwebassets.codepen.io/assets/embed/ei.js';
+
+			document.body.appendChild(script);
+		}
+	}
 </script>
+
+<Article
+	title={post.title}
+	date={post.display ? post.posted_at : false}
+	link={link ? `/blog/${post.slug}/` : false}
+>
+	<div class="content">
+		{@html post.html}
+	</div>
+
+	{#if post.display}
+		<div class="newsletter">
+			Interested in web development? Subscribe to my newsletter!
+			<NewsletterSubscribe />
+		</div>
+	{/if}
+</Article>
 
 <style>
 	.content :global(img) {
@@ -21,20 +55,3 @@
 		padding-top: 1em;
 	}
 </style>
-
-<Article
-	title={post.title}
-	date={post.display ? post.posted_at : false}
-	link={link ? `/blog/${post.slug}/` : false}>
-
-	<div class="content">
-		{@html post.html}
-	</div>
-
-	{#if post.display}
-		<div class="newsletter">
-			Interested in web development? Subscribe to my newsletter!
-			<NewsletterSubscribe />
-		</div>
-	{/if}
-</Article>
