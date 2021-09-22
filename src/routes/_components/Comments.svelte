b87c4a4e202927aafddde8a8a338b872b8b1c70e
Jesse Skinner <jesse@thefutureoftheweb.com>
2021-09-22 12:18:42 -0400

    NEW: refactor comments to separate component

---
+++ src/routes/_components/Comments.svelte
@@ -0,0 +1,65 @@
+<script>
+	import { formatDate } from '../_date.js';
+
+	export let comments;
+    export let url;
+</script>
+
+<section class="comments">
+	<h1>Comments</h1>
+	{#each comments as { title, author, avatar, date, body }, i}
+		<div id="comment{i + 1}" class="comment">
+			<h2>
+				<a href="{url}#comment{i + 1}">{i + 1}</a>
+				. {author} on {formatDate(date)}
+			</h2>
+			<img src={avatar} alt={author} />
+			<p>
+				{@html body}
+			</p>
+		</div>
+	{/each}
+
+	<div>
+		Comments are closed, but I'd still love to
+		<a href="/contact/me">hear your thoughts.</a>
+	</div>
+</section>
+
+<style>
+	.comments {
+		padding: 2em 3em;
+		margin-bottom: 3em;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+	}
+
+	.comment {
+		position: relative;
+		min-height: 130px;
+		margin-bottom: 1em;
+		max-width: 100%;
+		overflow: auto;
+	}
+
+	h2 {
+		margin: 0;
+		font-size: 100%;
+	}
+
+	p {
+		margin: 5px 0 0 25px;
+	}
+
+	img {
+		float: left;
+		width: 80px;
+		height: 80px;
+		margin: 10px 15px 5px 25px;
+	}
+
+	@media (min-width: 700px) {
+		.comments {
+			margin-right: 90px;
+		}
+	}
+</style>
--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -11,42 +11,41 @@
 	import Template from '../_Template.svelte';
 	import Post from '../_components/Post.svelte';
 	import AboutMe from '../_components/AboutMe.svelte';
-	import { formatDate } from '../_date.js';
+	import Comments from '../_components/Comments.svelte';
 
 	export let post;
 </script>
 
+<svelte:head>
+	<title>{post.title} - Coding with Jesse</title>
+
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content="@JesseSkinner" />
+	<meta property="og:title" content="{post.title} - Coding with Jesse" />
+	<meta
+		property="og:url"
+		content="https://www.codingwithjesse.com/blog/{post.slug}/"
+	/>
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:image"
+		content="https://www.codingwithjesse.com/images/background.gif"
+	/>
+</svelte:head>
+
+<Template>
+	<Post {post} link={false} />
+
+	{#if post.comments.length}
+		<Comments comments={post.comments} url="/blog/{post.slug}/" />
+	{/if}
+
+	<aside>
+		<AboutMe />
+	</aside>
+</Template>
+
 <style>
-	.comments {
-		padding: 2em 3em;
-		margin-bottom: 3em;
-		font-family: Verdana, Geneva, Tahoma, sans-serif;
-	}
-
-	.comment {
-		position: relative;
-		min-height: 130px;
-		margin-bottom: 1em;
-		max-width: 100%;
-		overflow: auto;
-	}
-
-	h2 {
-		margin: 0;
-		font-size: 100%;
-	}
-
-	p {
-		margin: 5px 0 0 25px;
-	}
-
-	img {
-		float: left;
-		width: 80px;
-		height: 80px;
-		margin: 10px 15px 5px 25px;
-	}
-
 	aside {
 		max-width: 380px;
 		margin: 2em auto;
@@ -59,56 +58,4 @@
 			right: 5vw;
 		}
 	}
-
-	@media (min-width: 700px) {
-		.comments {
-			margin-right: 90px;
-		}
-	}
 </style>
-
-<svelte:head>
-	<title>{post.title} - Coding with Jesse</title>
-
-	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:creator" content="@JesseSkinner" />
-	<meta property="og:title" content="{post.title} - Coding with Jesse" />
-	<meta
-		property="og:url"
-		content="https://www.codingwithjesse.com/blog/{post.slug}/" />
-	<meta property="og:type" content="website" />
-	<meta
-		property="og:image"
-		content="https://www.codingwithjesse.com/images/background.gif" />
-</svelte:head>
-
-<Template>
-	<Post {post} link={false} />
-
-	{#if post.comments.length}
-		<section class="comments">
-			<h1>Comments</h1>
-			{#each post.comments as { title, author, avatar, date, body }, i}
-				<div id="comment{i + 1}" class="comment">
-					<h2>
-						<a href="/blog/{post.slug}/#comment{i + 1}">{i + 1}</a>
-						. {author} on {formatDate(date)}
-					</h2>
-					<img src={avatar} alt={author} />
-					<p>
-						{@html body}
-					</p>
-				</div>
-			{/each}
-
-			<div>
-				Comments are closed, but I'd still love to
-				<a href="/contact/me">hear your thoughts.</a>
-			</div>
-		</section>
-	{/if}
-
-	<aside>
-		<AboutMe />
-	</aside>
-</Template>
